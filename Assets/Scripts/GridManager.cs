244a07cfbb5fcce5f243432ec6983d2902282d40
Khaffou Khaffou Mohamed <khaffoum@gmail.com>
2023-12-14 13:21:25 +0100

    creating grid and selector

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,6 +8,8 @@ public class GridManager : MonoBehaviour
     [SerializeField] private Tile _tilePrefab;
     [SerializeField] private Transform _cam;
 
+    private Dictionary<Vector2, Tile> _tiles;
+
     private void Start()
     {
         GenerateGrid();
@@ -15,6 +17,8 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, Tile>();
+
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _hight; y++)
@@ -25,9 +29,22 @@ public class GridManager : MonoBehaviour
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
 
                 instTile.Init(isOffset);
+
+                _tiles[new Vector2(x, y)] = instTile;
             }
         }
 
         _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_hight / 2 - 0.5f, -10);
+
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+
+        return null;
     }
 }
