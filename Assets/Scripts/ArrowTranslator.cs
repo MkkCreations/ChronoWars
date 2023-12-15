774ec818c8b98db3867212c18f2adec49afe2a99
Alex-C-IT <chanot.alexandre@gmail.com>
2023-12-15 12:06:10 +0100

    [ADD] Ajout d'une unité; [ADD] Algorithme A* (plus court chemin); [ADD] RangeFinder d'une unité; [ADD] Flèches de direction; [FIX] Correction de bugs

---
+++ Assets/Scripts/ArrowTranslator.cs
@@ -0,0 +1,92 @@
+using System;
+using UnityEngine;
+
+public class ArrowTranslator
+{
+    public enum ArrowDirection
+    {
+        None = 0,
+        UpDown = 1,
+        LeftRight = 2,
+        TopLeft = 3,
+        BottomLeft = 4,
+        TopRight = 5,
+        BottomRight = 6,
+        UpFinished = 7,
+        DownFinished = 8,
+        LeftFinished = 9,
+        RightFinished = 10
+    }
+
+    public ArrowDirection TranslateDirection(Tile previousTile, Tile currentTile, Tile futureTile)
+    {
+        bool isFinal = futureTile == null;
+
+        Vector2Int pastDirection = previousTile != null ? (currentTile.grid2DLocation - previousTile.grid2DLocation) : new Vector2Int(0, 0);
+        Vector2Int futureDirection = futureTile != null ? (futureTile.grid2DLocation - currentTile.grid2DLocation) : new Vector2Int(0, 0);
+        Vector2Int direction = pastDirection != futureDirection ? pastDirection + futureDirection : futureDirection;
+
+        if (direction == new Vector2(0, 1) && !isFinal)
+        {
+            return ArrowDirection.UpDown;
+        }
+
+        if (direction == new Vector2(0, -1) && !isFinal)
+        {
+            return ArrowDirection.UpDown;
+        }
+
+        if (direction == new Vector2(1, 0) && !isFinal)
+        {
+            return ArrowDirection.LeftRight;
+        }
+
+        if (direction == new Vector2(-1, 0) && !isFinal)
+        {
+            return ArrowDirection.LeftRight;
+        }
+
+        if (direction == new Vector2(1, 1))
+        {
+            return ArrowDirection.TopRight;
+        }
+
+        if (direction == new Vector2(-1, 1))
+        {
+            return ArrowDirection.TopLeft;
+        }
+
+        if (direction == new Vector2(1, -1))
+        {
+            return ArrowDirection.BottomRight;
+        }
+
+        if (direction == new Vector2(-1, -1))
+        {
+            return ArrowDirection.BottomLeft;
+        }
+
+        if (direction == new Vector2(0, 1) && isFinal)
+        {
+            return ArrowDirection.UpFinished;
+        }
+
+        if (direction == new Vector2(0, -1) && isFinal)
+        {
+            return ArrowDirection.DownFinished;
+        }
+
+        if (direction == new Vector2(-1, 0) && isFinal)
+        {
+            return ArrowDirection.LeftFinished;
+        }
+
+        if (direction == new Vector2(1, 0) && isFinal)
+        {
+            return ArrowDirection.RightFinished;
+        }
+
+        return ArrowDirection.None;
+    }
+}
+
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    // Instance unique de la classe
+    public static GridManager Instance;
     [SerializeField] private int _width, _hight;
     [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Unit _unitPrefab;
     [SerializeField] private Transform _cam;
 
-    private Dictionary<Vector2, Tile> _tiles;
+    private Dictionary<Vector2Int, Tile> _tiles;
 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     private void Start()
     {
         GenerateGrid();
@@ -17,7 +32,7 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
-        _tiles = new Dictionary<Vector2, Tile>();
+        _tiles = new Dictionary<Vector2Int, Tile>();
 
         for (int x = 0; x < _width; x++)
         {
@@ -26,11 +41,20 @@ public class GridManager : MonoBehaviour
                 var instTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
                 instTile.name = $"Tile {x} {y}";
 
+                // Placer une unité pour la team 1 en (0,0)
+                if (x == 5 && y == 5)
+                {
+                    var unit = Instantiate(_unitPrefab, new Vector3(x, y), Quaternion.identity);
+                    unit.name = $"Unit {x} {y}";
+                    unit.currentTile = instTile;
+                    instTile.SetUnite(unit.gameObject);
+                }
+
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
 
                 instTile.Init(isOffset);
 
-                _tiles[new Vector2(x, y)] = instTile;
+                _tiles[new Vector2Int(x, y)] = instTile;
             }
         }
 
@@ -38,7 +62,7 @@ public class GridManager : MonoBehaviour
 
     }
 
-    public Tile GetTileAtPosition(Vector2 pos)
+    public Tile GetTileAtPosition(Vector2Int pos)
     {
         if(_tiles.TryGetValue(pos, out var tile))
         {
@@ -47,4 +71,48 @@ public class GridManager : MonoBehaviour
 
         return null;
     }
+
+    public Dictionary<Vector2Int, Tile> map
+    {
+        get
+        {
+            return _tiles;
+        }
+        set
+        {
+            _tiles = value;
+        }
+    }
+
+    public List<Tile> GetSurroundingTiles(Vector2Int pos)
+    {
+        var tiles = new List<Tile>();
+
+        var tile = GetTileAtPosition(pos + Vector2Int.up);
+        if (tile != null)
+        {
+            tiles.Add(tile);
+        }
+
+        tile = GetTileAtPosition(pos + Vector2Int.down);
+        if (tile != null)
+        {
+            tiles.Add(tile);
+        }
+
+        tile = GetTileAtPosition(pos + Vector2Int.left);
+        if (tile != null)
+        {
+            tiles.Add(tile);
+        }
+
+        tile = GetTileAtPosition(pos + Vector2Int.right);
+        if (tile != null)
+        {
+            tiles.Add(tile);
+        }
+
+        return tiles;
+    }
+    
 }
